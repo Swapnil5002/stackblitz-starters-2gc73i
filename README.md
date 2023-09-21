190df26adfeb1aa4c093bbf8584dd63686849362
Swapnil5002 <52300853+Swapnil5002@users.noreply.github.com>
2023-09-21 23:42:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-2gc73i
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-2gc73i)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+import './style.css';
+
+export const App: FC = () => {
+  return <div>Beer BANK</div>;
+};
