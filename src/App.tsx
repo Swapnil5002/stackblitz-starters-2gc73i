fa60f33a6fd00fcafe264a99aaaeef9940039ebd
Swapnil5002 <52300853+Swapnil5002@users.noreply.github.com>
2023-09-22 00:41:20 +0530

    2 files modified, 3 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { FC, useState } from 'react';
+import { FC, useState, useRef, useEffect } from 'react';
 import { Input } from './components/input/input.component';
 
 import './style.css';
 
 export const App: FC = () => {
   const [inputVal, setInputVal] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef?.current?.focus();
+  }, []);
   const handleSearch = (e) => {
     let val = e.target.value;
     setInputVal(val);
   };
   return (
     <div>
-      Beer BANK
+      <h1>Beer BANK</h1>
+      <h5>Find your favourite beer here!</h5>
       <Input
+        inputRef={inputRef}
         type="text"
         placeholder="Search for Beer Name"
         value={inputVal}
         disabled={false}
         onChange={(e) => handleSearch(e)}
+        classes="bold"
       />
     </div>
   );
--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -4,28 +4,31 @@ interface InputProps {
   type: string;
   placeholder: string;
   value: string;
-  ref?: string;
+  inputRef?: React.RefObject<HTMLInputElement>;
   disabled: boolean;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  classes: string;
 }
 
 export const Input: React.FC<InputProps> = ({
   type,
   placeholder,
   value,
-  ref,
+  inputRef,
   disabled,
   onChange,
+  classes,
 }) => {
   return (
     <input
       id="1"
+      className={classes}
       type={type}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
       disabled={disabled}
-      ref={ref}
+      ref={inputRef}
     />
   );
 };
